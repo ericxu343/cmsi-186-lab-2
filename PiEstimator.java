f0273d68ef20e8b8d5333752aa4ace66752708ec
Eric Xu <ericxu343@gmail.com>
2020-02-06 19:33:19 -0500

    Adding code to SharedBirthday and PiEstimator

--- PiEstimator.java
+++ PiEstimator.java
@@ -7,14 +7,17 @@ public class PiEstimator {
             }
             //
             // TODO: Parse the command line argument and call your estimate function
-            //
+            int i = Integer.parseInt(args[0]);
+            System.out.println(estimate(i)); //checking if this 1000000 is a valid argument
         } catch (NumberFormatException e) {
             //
             // TODO: Take care of a possible non-integer argument.
+            System.err.println("You must enter a valid integer");
             //
         } catch (IllegalArgumentException e) {
             //
             // TODO: Take care of the exception you threw above.
+            System.err.println(e.getMessage());
             //
         }
     }
@@ -25,7 +28,35 @@ public class PiEstimator {
         // so the code compiles. It isn't right though. Remove the return here and
         // implement the whole method on your own.
         //
-        return 0.0;
+        int count = 0;
+        int radius = 1;
+        double proportion;
+        double estimate;
+
+        for(int i = 0; i < darts; i++) {
+          double x = Math.random(); // generates an x value from 0 to 1
+          double y = Math.random(); // generates a y value from 0 to 1
+          double X_square = Math.pow(x, 2); // (0,0) is the center
+          double Y_square = Math.pow(y, 2); // (o,0) is the center
+          double distance = Math.sqrt(X_square + Y_square); //calculates the distance
+        //  double proportion;
+        //  double estimate;
+
+          if (distance < radius) { // checks to see if the point is in the radius
+            count += 1; // increment in the count when it is in circle
+          //  System.out.println(count);
+          }
+
+        //  proportion = (double)count / i; // finds the proportion of bullseye to total
+          // estimate = 4 * proportion; // estimate PI
+
+          }
+        System.out.println(count);
+        proportion = (double)count/ darts;
+        estimate = 4 * proportion; // estimate PI
+        System.out.println("The estimate is " + Double.toString(estimate));
+        return estimate;
+
     }
 
     //
--- SharedBirthday.java
+++ SharedBirthday.java
@@ -1,3 +1,6 @@
+import java.util.*;
+// import java.time.temporal.ChronoUnit;
+
 public class SharedBirthday {
 
     public static void main(String[] args) {
@@ -5,9 +8,11 @@ public class SharedBirthday {
             if (args.length != 3) {
                 throw new IllegalArgumentException("Exactly three arguments required");
             }
+
             int people = Integer.parseInt(args[0]);
             int days = Integer.parseInt(args[1]);
             int trials = Integer.parseInt(args[2]);
+
             System.out.println(probabilityEstimate(people, days, trials));
         } catch (NumberFormatException e) {
             System.err.println("Arguments must all be integers");
@@ -22,7 +27,33 @@ public class SharedBirthday {
         // so the code compiles. It isn't right though. Remove the return here and
         // implement the whole method on your own.
         //
-        return 0.0;
+        if (people < 2) {
+          throw new IllegalArgumentException("At least two people required");
+        }
+        else if (days < 1) {
+          throw new IllegalArgumentException("At least one day required");
+        } else if (trials < 1) {
+          throw new IllegalArgumentException("At least one trial required");
+        }
+        int count = 0; // initializes the count at 0
+        for (int i = 0; i < trials; i++){ // counts the number of trials
+          HashSet<Integer> b = new HashSet<Integer>(); // creates a Hashset
+          for (int j = 0; j < people; j++){
+            boolean doesContain;
+            String bDay;
+            int day = (int)Math.floor(Math.random() * days); // generate a day
+            doesContain = b.contains(day); // takes the boolean value
+            if (doesContain == true){
+              count += 1; // if it contains (there's a match) then increment
+              break;
+            } else {
+              b.add(day); // add the day to data
+            }
+          }
+
+        }
+        return (double)count / (double)trials;
+
     }
 
     //
